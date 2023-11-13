4bef0a3bfd0bd097c3778d8b921430d1f4118484
iago-monteirog <iagomgarcia9000@gmail.com>
2023-11-13 20:31:02 -0300

    hashset

---
+++ java-foundations/Collections.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Collections {
+  public static void main(String[] args) {
+    Map<String, Integer> notas = new HashMap<>();
+    notas.put("Dani", 9);
+    notas.put("Vini", 10);
+    notas.put("Iago", 10);
+
+    for (Map.Entry<String, Integer> entry : notas.entrySet()) {
+      String key = entry.getKey();
+      Integer value = entry.getValue();
+
+      System.out.println("A chave e " + key + " e o valor e " + value);
+    }
+  }
+}
---
+++ java-foundations/HashSetCollection.java
@@ -0,0 +1,21 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashSetCollection {
+  public static void main(String[] args) {
+    Set<Integer> numbers = new HashSet<>();
+
+    numbers.add(10);
+    numbers.add(11);
+    numbers.add(12);
+    numbers.add(13);
+
+    // numbers.remove(12);
+
+    // for (Integer element : numbers) {
+    // System.out.println(element);
+    // }
+    boolean hasNumber = numbers.contains(13);
+    System.out.println(hasNumber);
+  }
+}
