b221e72abb1e396e43132300bbe919e2ee2eadc8
iago-monteirog <iagomgarcia9000@gmail.com>
2023-12-10 11:38:58 -0300

    fix username regex validation

--- gestao_vagas/src/main/java/br/com/iagogarcia/gestao_vagas/modules/candidate/CandidateEntity.java
+++ gestao_vagas/src/main/java/br/com/iagogarcia/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -5,7 +5,6 @@ import java.util.UUID;
 import org.hibernate.validator.constraints.Length;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
@@ -15,7 +14,7 @@ public class CandidateEntity {
   private UUID id;
   private String name;
 
-  @Pattern(regexp = "^(\\S+", message = "O campo username não deve conter espaços.")
+  @Pattern(regexp = "^(\\S+)$", message = "O campo username não deve conter espaços.")
   private String username;
 
   @Email(message = "O campo deve conter um e-mail válido.")
